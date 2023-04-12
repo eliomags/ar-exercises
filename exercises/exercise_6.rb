01fe367331f618f0e26ea2b6055a862858ee1eeb
Elshan Magsudov <elshanmagsudov@Elshans-MacBook-Pro.local>
2023-04-12 04:29:16 -0600

    ex 6-7

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,20 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+# 3. Add some data into employees. Here's an example of one (note how it differs from how you create stores): 
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+# 4. Go ahead and create some more employees using the create method. You can do this by making multiple calls to create (like you have before.) No need to assign the employees to variables though. Create them through the `@store#` instance variables that you defined in previous exercises. Create a bunch under `@store1` (Burnaby) and `@store2` (Richmond). Eg: `@store1.employees.create(...)`.
+@store1.employees.create(first_name: "John", last_name: "Doe", hourly_rate: 60)
+@store1.employees.create(first_name: "Jane", last_name: "Doe", hourly_rate: 60)
+@store1.employees.create(first_name: "Joe", last_name: "Doe", hourly_rate: 60)
+@store1.employees.create(first_name: "Aliuce", last_name: "Smith", hourly_rate: 60)
+@store1.employees.create(first_name: "Bob", last_name: "Brown", hourly_rate: 60)
+
+@store2.employees.create(first_name: "Michael", last_name: "Jackson", hourly_rate: 60)
+@store2.employees.create(first_name: "Elon", last_name: "Mask", hourly_rate: 60)
+@store2.employees.create(first_name: "Steve", last_name: "Jobs", hourly_rate: 60)
+@store2.employees.create(first_name: "Jack", last_name: "Darcey", hourly_rate: 60)
+@store2.employees.create(first_name: "Jon", last_name: "Hale", hourly_rate: 60)
+
+puts "There are #{Employee.count} employees in the database."
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,20 @@
 class Store < ActiveRecord::Base
+    # 1. Add the following code _directly_ inside the Store model (class): `has_many :employees`
+    has_many :employees
+
+    # 2. Add validations to two models to enforce the following business rules:
+    # - Stores must always have a name that is a minimum of 3 characters
+    validates :name, length: { minimum: 3 }
+    # - Stores have an annual_revenue that is a number (integer) that must be 0 or more
+    validates :annual_revenue, numericality: { greater_than_or_equal_to: 0 }
+    # - BONUS: Stores must carry at least one of the men's or women's apparel (hint: use a [custom validation method](http://guides.rubyonrails.org/active_record_validations.html#custom-methods) - **don't** use a `Validator` class)
+    validate :must_carry_mens_or_womens_apparel
+    
+    def must_carry_mens_or_womens_apparel
+      if mens_apparel == false && womens_apparel == false
+        errors.add(:Base, "Stores must carry at least one of men's or women's apparel")
+      end
+    end
+
 
 end
