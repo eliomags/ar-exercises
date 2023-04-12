3abccb965beed050bbc5ca69a3ffcf7789430e9d
Elshan Magsudov <elshanmagsudov@Elshans-MacBook-Pro.local>
2023-04-12 03:27:08 -0600

    excercises 1-3

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,11 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+# 1. Load the first store (with `id = 1`) from the database and assign it to an instance variable `@store1`.
+@store1 = Store.find(1)
+# 2. Load the second store from the database and assign it to `@store2`.
+@store2 = Store.find(2)
+# 3. Update the first store (`@store1`) instance in the database. (Change its name or something.)
+@store1.update(name: 'New Burnaby')
+puts @store1.name
--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,9 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+
+# 1. Load the third store (into `@store3`) as you did the other two before.
+@store3 = Store.find(3)
+# 2. Using Active Record's `destroy` method, delete the store from the database.
+@store3.destroy
+puts "There are #{Store.count} stores in the database."
