c1486db28c2ada5636578fb2100a1b11b9c5d5d3
Elshan Magsudov <elshanmagsudov@Elshans-MacBook-Pro.local>
2023-04-12 04:33:30 -0600

    fixed exercise #7

--- lib/store.rb
+++ lib/store.rb
@@ -1,7 +1,6 @@
 class Store < ActiveRecord::Base
     # 1. Add the following code _directly_ inside the Store model (class): `has_many :employees`
     has_many :employees
-
     # 2. Add validations to two models to enforce the following business rules:
     # - Stores must always have a name that is a minimum of 3 characters
     validates :name, length: { minimum: 3 }
@@ -9,12 +8,13 @@ class Store < ActiveRecord::Base
     validates :annual_revenue, numericality: { greater_than_or_equal_to: 0 }
     # - BONUS: Stores must carry at least one of the men's or women's apparel (hint: use a [custom validation method](http://guides.rubyonrails.org/active_record_validations.html#custom-methods) - **don't** use a `Validator` class)
     validate :must_carry_mens_or_womens_apparel
-    
+
     def must_carry_mens_or_womens_apparel
-      if mens_apparel == false && womens_apparel == false
-        errors.add(:Base, "Stores must carry at least one of men's or women's apparel")
-      end
+        if !mens_apparel && !womens_apparel
+            # errors.add(:mens_apparel, "must carry mens or womens apparel")
+            # errors.add(:womens_apparel, "must carry mens or womens apparel")
+            errors.add(:Base, "Stores must carry at least one of men's or women's apparel")
+        end
     end
-
-
+    
 end
